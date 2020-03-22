fcccabad7c94bfb357ed3e6f588ed67bb20b49ea
Annie T Bellefleur <anniebellefleur@gmail.com>
2020-03-22 18:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var recipes = {cake: '4 tier'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object[key] = value);
   
-  return object;
+  return Object.assign({}, object, {[key]: value});
 }
 
 updateObjectWithKeyAndValue(recipes, 'Cupcakes', '4 boxes')
